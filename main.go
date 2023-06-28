8634565eaefdd858bc538e53c00811bb9dd89026
Erkan Zileli <erkanzileli@gmail.com>
2023-06-29 00:18:20 +0300

    feat: initial commit
    Signed-off-by: Erkan Zileli <erkanzileli@gmail.com>

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/erkanzileli/kotification/pkg/controller"
+	"github.com/erkanzileli/kotification/pkg/reconciler"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"log"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
+)
+
+var (
+	gvk = schema.GroupVersionKind{
+		Group:   "networking.istio.io",
+		Version: "v1alpha3",
+		Kind:    "VirtualService",
+	}
+	expression = `any(spec.gateways, "mesh")`
+)
+
+func main() {
+	ctx := signals.SetupSignalHandler()
+
+	kubeClientConf := config.GetConfigOrDie()
+	managerConf := manager.Options{
+		LeaderElection:          true,
+		LeaderElectionID:        "kotification",
+		LeaderElectionNamespace: "default",
+	}
+	mgr, err := manager.New(kubeClientConf, managerConf)
+	if err != nil {
+		log.Fatalf("failed to create a new manager for creating controllers: %+v", err)
+	}
+
+	gvkController, err := controller.NewUnmanaged(mgr, gvk, controller.Options{
+		Reconciler: reconciler.NewGenericReconciler(gvk, expression, mgr.GetCache()),
+	})
+
+	if err = mgr.Add(gvkController); err != nil {
+		log.Fatalf("failed to register controller to manager: %+v", err)
+	}
+
+	log.Print("controller added to manager")
+
+	// Entrypoint
+	if err = mgr.Start(ctx); err != nil {
+		log.Fatalf("failed to start all registered controllers: %+v", err)
+	}
+}
